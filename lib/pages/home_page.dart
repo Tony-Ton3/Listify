26b63ce376d57dcd34700eeeb5e537657d2b6783
Numan Khan <101899800+numan-7@users.noreply.github.com>
2023-04-17 19:32:01 -0700

    Updated home.dart
    + can views user specific tasks
    + can delete by swiping right? some bugs to fix
    + soon be able to edit by long pressing
    
    Co-Authored-By: Tony-Ton3 <95826645+Tony-Ton3@users.noreply.github.com>

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,42 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'add.dart';
+import 'edit.dart';
+
+class Task {
+  final String id;
+  final String title;
+  bool completed;
+  String imageUrl;
+
+  Task({
+    String? id,
+    required this.title,
+    required this.completed,
+    required this.imageUrl,
+  }) : id = id ?? ''; // Assigns an empty string as the default ID value if null
+
+  factory Task.fromMap(Map<String, dynamic> map, String id) {
+    // A factory constructor that takes a map of task properties and an ID as arguments
+    return Task(
+      id: id,
+      title: map['title'] ?? '',
+      completed: map['completed'] ?? false,
+      imageUrl: map['imageUrl'] ?? '',
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    // A method that returns a map of task properties
+    return {
+      'id': id,
+      'title': title,
+      'completed': completed,
+      'imageUrl': imageUrl,
+    };
+  }
+}
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -18,6 +54,12 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser!;
+    final userId = user?.uid;
+
+    final CollectionReference tasks =
+        FirebaseFirestore.instance.collection('users/$userId/tasks');
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('My Tasks'),
@@ -28,13 +70,103 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("signed in as: ${user.email!}"),
-          ],
-        ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: tasks.snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          // Extract the documents from the QuerySnapshot and convert them to Task objects
+          final List<Task> taskList = snapshot.data!.docs
+              .map((QueryDocumentSnapshot doc) =>
+                  Task.fromMap(doc.data() as Map<String, dynamic>, doc.id))
+              .toList();
+          // Show the list of tasks in a ListView widget
+          return ListView.builder(
+            itemCount: taskList.length,
+            itemBuilder: (BuildContext context, int index) {
+              final Task task = taskList[index];
+
+              return Dismissible(
+                key: Key(task.id),
+                direction: DismissDirection.endToStart,
+                background: Container(
+                  color: Colors.red,
+                  child: const Align(
+                    alignment: Alignment.centerRight,
+                    child: Padding(
+                      padding: EdgeInsets.only(right: 16),
+                      child: Icon(Icons.delete, color: Colors.white),
+                    ),
+                  ),
+                ),
+                onDismissed: (_) async {
+                  await tasks.doc(task.id).delete();
+                },
+                child: ListTile(
+                  trailing: Checkbox(
+                    value: task.completed,
+                    onChanged: (bool? value) async {
+                      if (value != null) {
+                        await tasks.doc(task.id).update({'completed': value});
+                      }
+                    },
+                  ),
+                  title: Opacity(
+                    opacity: task.completed ? 0.5 : 1.0,
+                    child: Text(task.title),
+                  ),
+                  subtitle: Opacity(
+                    opacity: task.completed ? 0.5 : 1.0,
+                    child: Text(task.completed ? 'Completed' : 'Incomplete'),
+                  ),
+                  leading: task.imageUrl.isNotEmpty
+                      ? Opacity(
+                          opacity: task.completed ? 0.5 : 1.0,
+                          child: GestureDetector(
+                            onTap: () {
+                              showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return Dialog(
+                                    child: Stack(
+                                      children: <Widget>[
+                                        SizedBox(
+                                          width: double.infinity,
+                                          height: 300,
+                                          child: Image.network(task.imageUrl),
+                                        ),
+                                        const Positioned(
+                                          top: 0,
+                                          right: 0,
+                                          child: CloseButton(),
+                                        ),
+                                      ],
+                                    ),
+                                  );
+                                },
+                              );
+                            },
+                            child: CircleAvatar(
+                              backgroundImage: NetworkImage(task.imageUrl),
+                            ),
+                          ),
+                        )
+                      : const Icon(Icons.image),
+                  onTap: () async {
+                    final bool completed = !task.completed;
+                    await tasks.doc(task.id).update({'completed': completed});
+                  },
+                  onLongPress: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => (const HomePage()),
+                    ));
+                  },
+                ),
+              );
+            },
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
