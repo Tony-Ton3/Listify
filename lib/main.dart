db5ef74074f7d433bd0df3590a999ed15fe8bc37
Tony-Ton3 <95826645+Tony-Ton3@users.noreply.github.com>
2023-05-04 13:26:58 -0700

    code comments

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,13 @@ import 'package:learn_firebase/firebase_options.dart';
 import 'authentication/main_page.dart';
 
 void main() async {
+//makes sure flutter framwork is initialized before initializing firebase
   WidgetsFlutterBinding.ensureInitialized();
+  //initializes firebase with default options for current platform 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  //continusully access firebase to our application
+  //creating instance of MyApp widget 
   runApp(
     const MyApp(),
   );
@@ -17,11 +19,12 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+//build would be called wherever the app needs to be rebuilt creating a new instance of MaterialApp
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = ThemeData();
     return MaterialApp(
-      home: const MainPage(),
+      home: const MainPage(), 
       debugShowCheckedModeBanner: false,
       theme: theme.copyWith(
         colorScheme: theme.colorScheme.copyWith(
