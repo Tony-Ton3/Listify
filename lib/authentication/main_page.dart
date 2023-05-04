891bbc3bf6a987ba929e3bcc136befcfc79a215b
Tony-Ton3 <95826645+Tony-Ton3@users.noreply.github.com>
2023-05-04 13:50:24 -0700

    code comment

--- lib/authentication/main_page.dart
+++ lib/authentication/main_page.dart
@@ -9,13 +9,16 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //StreamBuilder takes a stream of data as its input 
       body: StreamBuilder<User?>(
+        //fill stream with authStateChanges from Firebase auth
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
-          //checks if user is already logged in or not
-          if (snapshot.hasData) {
+          //checks if user is already logged in or not by taking a snapshot of stream 
+          //and seeing if there is a event received on the stream
+          if (snapshot.hasData) { //if snapshot has data it means user has already logged in 
             return const HomePage();
-          } else {
+          } else { //otherwise bring user to authPage
             return const AuthPage();
           }
         },
